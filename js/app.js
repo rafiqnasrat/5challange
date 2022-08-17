60696a303a508143fea4c9e2b21782be8c64a6a2
rafiqnasrat <rafiqullahnasrat3@gmail.com>
2022-08-17 14:47:46 -0500

    i use each loop withe j quri

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+// date for title 
+
 $(document).ready(function(){
     $("#date").html(moment().format("MMMM, Do ,YYYY"));
 });
+
+// Object
+var now = new Date();
+// now.setHours("")
+// var for now time 
+var hour = now.getHours();
+
+$(".event").each(function(){
+    // checking for past 
+    if(hour > $(this).data("time"))
+    {
+        $(this).attr("class", "past");
+
+    }
+
+    if(hour === $(this).data("time"))
+    {
+    $(this).attr("class", "present");
+}
+if(hour < $(this).data("time"))
+{
+    $(this).attr("class", "future");
+}
+    
+})
+
+
+
+
+$(".saveBtn").click(function(e){
+    var textarea = e.target.parentElement.parentElement.querySelector("textarea");
+    var key = textarea.getAttribute("data-time");
+    var value = textarea.value;
+})
+
+
